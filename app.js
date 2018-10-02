542b8a7c28c9b5781431b12589b27596e94b6f12
pierre vanneste <pierre@debian.invalid>
2018-10-02 16:42:03 +0200

    formulaire page d'accueil et nav bar

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
 "use strict";
-const express = require('express')
+const express = require("express");
 const app = express();
 const path = require("path");
+const server = require("http").Server(app);
+const session = require('express-session')
+const bodyParser = require("body-parser");
+const expressMongoDb = require("express-mongo-db");
+const MongoStore = require('connect-mongo')(session);
 
 // view engine setup
+app.locals.pretty = true;
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'))
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'))
 
-app.get('/', function (req, res) {
-    res.render('accueil', {
-        titre: 'BigPixel'
-    })
-})
+app.get("/", function (req, res) {
+    res.render("accueil", {
+        titre: "BigPixel"
+    });
+});
+app.get("/signUp", function (req, res) {
+    res.render("signUp", {
+        titre: "BigPixel",
+    });
+});
+
+app.get('/profil', function (req, res) {
+    let user = req.session.user
+    console.log(req.session.user)
+    res.render('profil', {
+        titre: "BigPixel",
+        user: user
+    });
+});
 
 app.listen(3000, function () {
-    console.log('Serveur connecté sur le port 3000');
-})
+    console.log("Serveur connecté sur le port 3000");
+});
--- routes/index.js
+++ routes/index.js
@@ -3,35 +3,49 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Express'
-  });
-})
-// .get('/profil', function (req, res, next) {
-//   console.log(req.db.collection('users').findOne({}, {
-//     sort: {
-//       _id: -1
-//     },
-//     limit: 1
-//   }));
-//   req.db.collection('users').findOne({}, {
-//     sort: {
-//       _id: -1
-//     },
-//     limit: 1
-//   });
-// req.db.collection('users').findOne({
-
-// })
-// res.render('profil', {
-//   firstname: firstname,
-//   lastname: lastname,
-//   username: username,
-//   adresse: adresse,
-//   age: age,
-//   email: email,
-//   username: req.body.username
-// })
-// })
-
+    res.render('index', {
+      title: 'Express'
+    });
+  })
+  .post('/form', function (req, res, next) {
+    console.log('azzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz')
+    var pseudo = req.body.pseudo.trim();
+    var email = req.body.email;
+    var password = req.body.password;
+    var city = req.body.city;
+    var movie = req.body.movie.trim();
+    var game = req.body.game.trim();
+    // /---/  MONGO  /---/ //
+    req.db.collection('utilisateurs').findOne({
+      pseudo: {
+        $regex: pseudo,
+        $options: "is"
+      },
+    }, function (err, result) {
+      if (result) {
+        res.render('form', {
+          error: true,
+          message: "Le pseudo est déja pris"
+        })
+      } else {
+        req.db.collection('utilisateurs').insertOne({
+            pseudo: pseudo,
+            password: password
+          },
+          err => {
+            if (err) {
+              throw err;
+            } else {
+              let user = {
+                pseudo: pseudo,
+                password: password
+              }
+              req.session.user = user
+              res.redirect("/profil");
+              console.log(req.body)
+            }
+          })
+      }
+    })
+  })
 module.exports = router;
