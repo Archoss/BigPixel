e44de66aa9fefcf243c64c489eb87d7027500b1f
pierre vanneste <pierre@debian.invalid>
2018-10-09 09:27:37 +0200

    derniere version à jour avec les informations de l'inscription qui vont dans le base de données pour êtres réutilisées sur la page profile

--- app.js
+++ app.js
@@ -1,21 +1,54 @@
 "use strict";
 const express = require("express");
+const session = require("express-session");
 const app = express();
 const path = require("path");
-const server = require("http").Server(app);
-const session = require('express-session')
 const bodyParser = require("body-parser");
+const server = require("http").Server(app);
 const expressMongoDb = require("express-mongo-db");
-const MongoStore = require('connect-mongo')(session);
+const MongoStore = require("connect-mongo")(session);
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
 
 // view engine setup
-app.locals.pretty = true;
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+app.set('trust proxy', 1) // trust first proxy
+app.locals.pretty = true;
+
+app.use(expressMongoDb("mongodb://127.0.0.1:27017/bigPixel_db"));
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'))
-app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'))
+app.use(
+    "/bootstrap",
+    express.static(__dirname + "/node_modules/bootstrap/dist/")
+);
+app.use("/jquery", express.static(__dirname + "/node_modules/jquery/dist/"));
+
+app.use(
+    session({
+        secret: "thisisasecretshhhh",
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+            secure: false
+        },
+        store: new MongoStore({
+            url: "mongodb://localhost:27017/bigPixel",
+            ttl: 30 * 60
+        })
+    })
+);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 app.get("/", function (req, res) {
     res.render("accueil", {
@@ -24,19 +57,21 @@ app.get("/", function (req, res) {
 });
 app.get("/signUp", function (req, res) {
     res.render("signUp", {
-        titre: "BigPixel",
+        titre: "BigPixel"
     });
 });
 
-app.get('/profil', function (req, res) {
-    let user = req.session.user
-    console.log(req.session.user)
-    res.render('profil', {
+app.get("/profile", function (req, res) {
+    let user = req.session.user;
+    console.log(req.session.user);
+    res.render("profile", {
         titre: "BigPixel",
         user: user
     });
 });
 
-app.listen(3000, function () {
+server.listen(3000, function () {
     console.log("Serveur connecté sur le port 3000");
 });
+
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,34 +3,54 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {
-      title: 'Express'
+    res.render('accueil', {
+      title: 'BigPixel'
     });
   })
-  .post('/form', function (req, res, next) {
-    console.log('azzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz')
+  .post('/signUp', function (req, res, next) {
+    console.log(req.body)
     var pseudo = req.body.pseudo.trim();
     var email = req.body.email;
     var password = req.body.password;
-    var city = req.body.city;
+    var lastname = req.body.lastname.trim();
+    var firstname = req.body.firstname.trim();
+    var age = req.body.age;
+    var adress = req.body.adress.trim();
+    var city = req.body.city.trim();
     var movie = req.body.movie.trim();
     var game = req.body.game.trim();
+    var presentation = req.body.presentation;
+    console.log("req.body.pseudo --> " + req.body.pseudo)
+    console.log("req.body.email --> " + req.body.email)
     // /---/  MONGO  /---/ //
     req.db.collection('utilisateurs').findOne({
       pseudo: {
         $regex: pseudo,
         $options: "is"
       },
+      email: {
+        $regex: email,
+        $options: "is"
+      },
     }, function (err, result) {
       if (result) {
-        res.render('form', {
+        res.render('signUp', {
           error: true,
-          message: "Le pseudo est déja pris"
+          message: "Le pseudo et/ou le mot de passe est déja pris"
         })
       } else {
         req.db.collection('utilisateurs').insertOne({
             pseudo: pseudo,
-            password: password
+            email: email,
+            password: password,
+            lastname: lastname,
+            firstname: firstname,
+            age: age,
+            adress: adress,
+            city: city,
+            movie: movie,
+            game: movie,
+            presentation: presentation,
           },
           err => {
             if (err) {
@@ -38,10 +58,19 @@ router.get('/', function (req, res, next) {
             } else {
               let user = {
                 pseudo: pseudo,
-                password: password
+                email: email,
+                password: password,
+                lastname: lastname,
+                firstname: firstname,
+                age: age,
+                adress: adress,
+                city: city,
+                movie: movie,
+                game: game,
+                presentation: presentation,
               }
               req.session.user = user
-              res.redirect("/profil");
+              res.redirect("/profile");
               console.log(req.body)
             }
           })
